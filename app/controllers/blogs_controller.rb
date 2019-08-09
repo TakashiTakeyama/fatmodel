84edad1cb20011e4360609c7f77d4a7c58e86676
Takashi <Takashi@gmail.com>
2019-08-09 14:57:03 +0900

    Service objectを利用しました

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -28,6 +28,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
+        ContentCleneupSurvice.new(@blog).call
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
         format.json { render :show, status: :created, location: @blog }
       else
@@ -42,6 +43,7 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
+        ContentCleneupSurvice.new(@blog).call
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
         format.json { render :show, status: :ok, location: @blog }
       else
